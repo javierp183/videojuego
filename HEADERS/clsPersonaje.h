131b66a8cb2b1c1e55e854f923fcb61293a6f50e
Javier Pereyra <javierp@ar.ibm.com>
2018-10-18 18:24:23 -0300

    Personaje de juego

---
+++ HEADERS/clsPersonaje.h
@@ -0,0 +1,15 @@
+#ifndef CLSPERSONAJE_H
+#define CLSPERSONAJE_H
+
+
+class clsPersonaje
+{
+    public:
+        clsPersonaje();
+
+    protected:
+
+    private:
+};
+
+#endif // CLSPERSONAJE_H
---
+++ SOURCES/clsPersonaje.cpp
@@ -0,0 +1,6 @@
+#include "clsPersonaje.h"
+
+clsPersonaje::clsPersonaje()
+{
+    //ctor
+}
