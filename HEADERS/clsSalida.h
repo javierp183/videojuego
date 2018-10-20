2511bbb1a18fabb33595bd9e60f0d0c12c850e8c
Javier Pereyra <javierp@ar.ibm.com>
2018-10-20 19:07:47 -0300

    clase salida y musica

---
+++ HEADERS/clsSalida.h
@@ -0,0 +1,18 @@
+#ifndef CLSSALIDA_H
+#define CLSSALIDA_H
+
+#include <clsSprite.h>
+
+
+class clsSalida : public clsSprite
+{
+    public:
+        clsSalida();
+        virtual ~clsSalida();
+
+    protected:
+
+    private:
+};
+
+#endif // CLSSALIDA_H
